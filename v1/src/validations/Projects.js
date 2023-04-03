26cea23070979d728f350b618ebb7770483612f2
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-04-03 13:30:09 +0300

    winston project and user validations and project logger

---
+++ v1/src/validations/Projects.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+const createValidation = Joi.object({
+  name: Joi.string().required().min(5),
+});
+
+const updateValidation = Joi.object({
+  name: Joi.string().required().min(5),
+});
+
+module.exports = {
+  createValidation,
+  updateValidation,
+};
---
+++ v1/src/validations/Users.js
@@ -0,0 +1,28 @@
+const Joi = require("joi");
+
+const createValidation = Joi.object({
+  full_name: Joi.string().required().min(2),
+  password: Joi.string().required().min(8),
+  email: Joi.string().email().required().min(8),
+});
+
+const loginValidation = Joi.object({
+  password: Joi.string().required().min(8),
+  email: Joi.string().email().required().min(8),
+});
+
+const resetPasswordValidation = Joi.object({
+  email: Joi.string().email().required().min(8),
+});
+
+const updateValidation = Joi.object({
+  full_name: Joi.string().min(2),
+  email: Joi.string().email().min(8),
+});
+
+module.exports = {
+  createValidation,
+  loginValidation,
+  resetPasswordValidation,
+  updateValidation,
+};

76cfc53376eea09f3d70cfe35d7b63e2ff80dea6
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-04-07 22:14:36 +0300

    use static files configuration

--- v1/src/app.js
+++ v1/src/app.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const helmet = require("helmet");
 const config = require("./config");
 const loaders = require("./loaders");
 const { ProjectRoutes, UserRoutes } = require("./routes");
 const events = require("./scripts/events");
+const path = require("path");
 
 config();
 loaders();
 events();
 
 const app = express();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(helmet());
+app.use(fileUpload());
 
 app.listen(process.env.APP_PORT, () => {
   console.log("SERVER RUNNING ON :" + process.env.APP_PORT);
